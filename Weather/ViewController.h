2250e99706f1ea6cab254b3346b65823434d0e43
youxin <>
2018-07-24 11:37:23 +0800

    一个很不错的开屏动画，原理动画+gif效果

---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by youxin on 2018/7/24.
+//  Copyright © 2018年 yst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
